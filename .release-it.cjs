d2a57a34191a024c9b519348b4b79df113841857
kali <kalivivian@qq.com>
2026-05-10 23:48:37 +0800

    chore: 将发布工具从 semantic-release 迁移到 release-it
    添加 release-it 配置文件并更新 package.json 中的相关脚本和依赖项
    移除不再需要的 semantic-release 配置文件和相关依赖

---
+++ .release-it.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  git: {
+    requireCleanWorkingDir: true,
+    requireUpstream: true,
+    requireBranch: false,
+    requireCommits: true,
+    commit: true,
+    commitMessage: 'chore: release v${version}',
+    tag: true,
+    tagName: 'v${version}',
+    tagAnnotation: 'v${version}\n\n${changelog}',
+    push: true,
+    pushArgs: ['--follow-tags']
+  },
+
+  npm: {
+    publish: false
+  },
+
+  github: {
+    release: false
+  },
+
+  gitlab: {
+    release: false
+  },
+
+  hooks: {
+    'before:init': 'git fetch --tags origin'
+  },
+
+  plugins: {
+    '@release-it/conventional-changelog': {
+      infile: 'CHANGELOG.md',
+      preset: 'conventionalcommits'
+    }
+  }
+};
